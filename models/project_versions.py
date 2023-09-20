18c076b4d84c2a5ef1c32c583923adf9bcdba45c
Cela24k <62605203+Cela24k@users.noreply.github.com>
2023-09-20 13:14:14 +0200

    project_version: updated model

--- models/project_versions.py
+++ models/project_versions.py
@@ -6,7 +6,6 @@ class VersionProject(db.Model):
     __tablename__ = 'version_projects'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.Text, nullable=False)
     status = db.Column(db.Enum(ProjectStatus), nullable=False)
     project_id = db.Column(db.Integer, db.ForeignKey('projects.id'))
+    version = db.Column(db.String, nullable=False)
