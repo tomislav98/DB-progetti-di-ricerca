bea3977f07b182681acf4f9d9144d141e452bd35
Cela24k <62605203+Cela24k@users.noreply.github.com>
2023-09-25 15:31:02 +0200

    Added react boiler code and auth page

---
+++ client/react-project/src/Components/SignIn/SignIn.jsx
@@ -0,0 +1,38 @@
+
+import React from 'react';
+import './sign-in.scss';
+import unive from '../../assets/Cat Icon 1.svg'
+
+function Sign() {
+  return (
+    <div className="my-container">
+      <div className="text-center my-container-flex">
+        <main className="form-signin">
+          <form>
+            <img className="mb-4" src={unive} alt="" width="72" height="57"></img>
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+            <div className="form-floating">
+              <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+              <label htmlFor="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating">
+              <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+              <label htmlFor="floatingPassword">Password</label>
+            </div>
+
+            <div className="checkbox mb-3">
+              <label>
+                <input type="checkbox" value="remember-me" /> Remember me
+              </label>
+            </div>
+            <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+            <p className="mt-5 mb-3 text-muted">&copy; 2022–2023</p>
+          </form>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Sign;
