6cdc9ead245070822ba5deeac7a28af4316b71b2
Cela24k <882778@stud.unive.it>
2023-10-09 22:40:34 +0200

    Added no projects warning
    instead of leaving blank and other style fixes

--- client/react-project/src/Components/MainPage/Project/Projects.jsx
+++ client/react-project/src/Components/MainPage/Project/Projects.jsx
@@ -2,7 +2,7 @@ import './project.scss';
 import BasicModal from './CreateProjectStepper/Modal';
 import { IconSearch, IconArrowRight } from '@tabler/icons-react';
 import { useState, useEffect } from 'react';
-import { Card, Avatar, Text, Progress, Badge, Group, ActionIcon, TextInput, useMantineTheme, rem, UnstyledButton, Menu, Image, RingProgress, Center } from '@mantine/core';
+import { Card, Avatar, Text, Progress, Badge, Group, ActionIcon, TextInput, useMantineTheme, rem, UnstyledButton, Menu, Image, RingProgress, Center, Container, Title, Button } from '@mantine/core';
 import { IconUpload } from '@tabler/icons-react';
 import axios from 'axios';
 import { IconChevronDown } from '@tabler/icons-react';
@@ -16,6 +16,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsisVertical, faCircle } from '@fortawesome/free-solid-svg-icons';
 import { Routes, Route } from 'react-router-dom';
 import SingleProject from './SingleProject/SingleProject';
+import catImage from '../../../assets/errorimage3.png'
+import classes from './NothingFoundBackground.module.css';
 
 const avatars = [
     'https://avatars.githubusercontent.com/u/10353856?s=460&u=88394dfd67727327c1f7670a1764dc38a8a24831&v=4',
@@ -113,7 +115,7 @@ const BootstrapTooltip = styled(({ className, ...props }) => (
 
 function ProjectCard({ name, description, id, status, version = 'v0.0.0', username }) {
     return (
-        <Card className='justify-content-around my-card'  padding="lg" radius="md" style={{ height: '300px' }}>
+        <Card className='justify-content-around my-card' padding="lg" radius="md" style={{ height: '300px' }}>
             <Group justify="space-between">
 
                 <div className='d-flex'>
@@ -151,8 +153,39 @@ function ProjectCard({ name, description, id, status, version = 'v0.0.0', userna
     );
 }
 
+export function Illustration(props) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 362 145" {...props}>
+            <path
+                fill="white"
+                d="M62.6 142c-2.133 0-3.2-1.067-3.2-3.2V118h-56c-2 0-3-1-3-3V92.8c0-1.333.4-2.733 1.2-4.2L58.2 4c.8-1.333 2.067-2 3.8-2h28c2 0 3 1 3 3v85.4h11.2c.933 0 1.733.333 2.4 1 .667.533 1 1.267 1 2.2v21.2c0 .933-.333 1.733-1 2.4-.667.533-1.467.8-2.4.8H93v20.8c0 2.133-1.067 3.2-3.2 3.2H62.6zM33 90.4h26.4V51.2L33 90.4zM181.67 144.6c-7.333 0-14.333-1.333-21-4-6.666-2.667-12.866-6.733-18.6-12.2-5.733-5.467-10.266-13-13.6-22.6-3.333-9.6-5-20.667-5-33.2 0-12.533 1.667-23.6 5-33.2 3.334-9.6 7.867-17.133 13.6-22.6 5.734-5.467 11.934-9.533 18.6-12.2 6.667-2.8 13.667-4.2 21-4.2 7.467 0 14.534 1.4 21.2 4.2 6.667 2.667 12.8 6.733 18.4 12.2 5.734 5.467 10.267 13 13.6 22.6 3.334 9.6 5 20.667 5 33.2 0 12.533-1.666 23.6-5 33.2-3.333 9.6-7.866 17.133-13.6 22.6-5.6 5.467-11.733 9.533-18.4 12.2-6.666 2.667-13.733 4-21.2 4zm0-31c9.067 0 15.6-3.733 19.6-11.2 4.134-7.6 6.2-17.533 6.2-29.8s-2.066-22.2-6.2-29.8c-4.133-7.6-10.666-11.4-19.6-11.4-8.933 0-15.466 3.8-19.6 11.4-4 7.6-6 17.533-6 29.8s2 22.2 6 29.8c4.134 7.467 10.667 11.2 19.6 11.2zM316.116 142c-2.134 0-3.2-1.067-3.2-3.2V118h-56c-2 0-3-1-3-3V92.8c0-1.333.4-2.733 1.2-4.2l56.6-84.6c.8-1.333 2.066-2 3.8-2h28c2 0 3 1 3 3v85.4h11.2c.933 0 1.733.333 2.4 1 .666.533 1 1.267 1 2.2v21.2c0 .933-.334 1.733-1 2.4-.667.533-1.467.8-2.4.8h-11.2v20.8c0 2.133-1.067 3.2-3.2 3.2h-27.2zm-29.6-51.6h26.4V51.2l-26.4 39.2z"
+            />
+        </svg>
+    );
+}
 
-function ProjectContainer({onProjectChange}) {
+export function NothingFoundBackground() {
+    return (
+        <Container className={classes.mantineroot}>
+            <div className={classes.mantineinner}>
+                <Illustration className={classes.mantineimage} />
+                <div className={classes.mantinecontent}>
+                    <Title className={classes.mantinetitle} style={{marginBottom:'35px'}}>Nothing to see here</Title>
+                    <Text classNames='' c="dimmed" size="lg" ta="center" className={classes.mentinedescription} style={{margin:'10px', marginBottom:'25px'}}>
+                        Welcome! It seems like you haven't started any projects yet,
+                        but don't worry,
+                        you can kickstart your journey by simply clicking the inviting button below to create your very first project.
+                    </Text>
+                    <Group classNames='' justify="center">
+                        <Button size="md">Create a new project</Button>
+                    </Group>
+                </div>
+            </div>
+        </Container>
+    );
+}
+
+function ProjectContainer({ onProjectChange }) {
     const [projects, setProjects] = useState([]);
     const matches = useMediaQuery('(min-width:600px)', { noSsr: true });
     const navigate = useNavigate();
@@ -186,7 +219,7 @@ function ProjectContainer({onProjectChange}) {
                 let changesCount = 0;
                 let submittedCount = 0;
 
-                
+
                 for (const project of response.data) {
                     if (project.status === 'ProjectStatus.APPROVED') {
                         approvedCount++;
@@ -219,8 +252,8 @@ function ProjectContainer({onProjectChange}) {
             });
     }
 
-    function handleCardClick(id){
-        navigate('/mainpage/projects/'+id);
+    function handleCardClick(id) {
+        navigate('/mainpage/projects/' + id);
     }
 
     useEffect(() => {
@@ -233,6 +266,7 @@ function ProjectContainer({onProjectChange}) {
             <div className='row my-full-row justify-content-around'>
                 <div className='col-12 col-lg-9'>
                     <div className='container projects-container'>
+
                         <div className='row content-row'>
                             <div className='col-12 col-lg-5 '>
                                 <InputWithButton />
@@ -246,7 +280,7 @@ function ProjectContainer({onProjectChange}) {
                                     <div className='col-4 d-flex align-items-center'>
                                         <div className='row'>
                                             {matches ?
-                                                <div className='col-4'>
+                                                <div className='col-4 d-flex align-items-center'>
                                                     <p className='text-muted small' style={{ margin: 0 }}> Sort by</p>
                                                 </div> :
                                                 null
@@ -258,21 +292,24 @@ function ProjectContainer({onProjectChange}) {
                                     </div>
                                 </div>
                             </div>
-                            <div className='col-12'>
-                                <div className='row cards-row'>
-                                    {projects.map((proj, i) => {
-                                        return (
-                                            <div className='col-12 col-md-6 col-lg-4' key={i} pid={proj.id} onClick={()=>{handleCardClick(proj.id)}}>
-                                                <ProjectCard name={proj.name} description={proj.description} id={proj.id} status={proj.status} username={username} version={proj.version} key={i}   />
-                                            </div>
-                                        );
-                                    })}
-                                </div>
-                            </div>
+
+                        </div>
+                        <div className='row cards-row'>
+                            {projects.length === 0 ?
+                                <NothingFoundBackground />
+                                :
+                                projects.map((proj, i) => {
+                                    return (
+                                        <div className='col-12 col-md-6 col-lg-4' key={i} pid={proj.id} onClick={() => { handleCardClick(proj.id) }}>
+                                            <ProjectCard name={proj.name} description={proj.description} id={proj.id} status={proj.status} username={username} version={proj.version} key={i} />
+                                        </div>
+                                    );
+                                })
+                            }
                         </div>
                     </div>
                 </div>
-                <div className='col-12 col-lg-3'>
+                <div className='col-12 col-lg-3' style={{ height: '100%' }}>
                     <div className='total-main-container align-items-center'>
                         <div className='row half-page-container'>
                             <div className='row total-title-row align-items-end'>
@@ -371,7 +408,7 @@ function ProjectContainer({onProjectChange}) {
 function Projects() {
     const [projects, setProjects] = useState([]);
 
-    function updateProjects(projects){
+    function updateProjects(projects) {
         setProjects(projects);
     }
 
