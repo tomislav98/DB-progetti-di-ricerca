10b0c869ce58da7068421d1a9b434c35a36fbfbe
Mimiview <57045037+Mimiview@users.noreply.github.com>
2023-11-08 19:08:22 +0100

    fix: admin panel changes

--- client/react-project/src/Components/MainPage/Admin/AdminPanel.jsx
+++ client/react-project/src/Components/MainPage/Admin/AdminPanel.jsx
@@ -1,6 +1,53 @@
-import { Calendar } from '@mantine/dates';
+import { useState } from 'react';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import './AdminPanel.scss'
+import { ScrollArea } from '@mantine/core';
+import { faGauge, faNoteSticky, faCalendar } from '@fortawesome/free-solid-svg-icons';
+import { LinksGroup } from '../Project/SingleProject/LinksGroup';
+
 
 export default function AdminPanel() {
-  
-    return <Calendar />;
+    const mockdata = [
+      { label: 'Reports', icon: faGauge, onClickedLink: ()=> {console.log("mimmo fiocosa")}  }, // per vedere i report dell ultima versione, senno fare un tasto sulla versione
+      {
+          label: 'Update',
+          icon: faNoteSticky,
+          initiallyOpened: true,
+          links: [
+              { label: 'Project data', link: '/' },
+              { label: 'Single document', link: '/' },
+          ],
+      },
+      {
+          label: 'Evaluation Window',
+          icon: faCalendar,
+          onClickedLink: ()=>{
+            alert("msdafnasdfj")
+          }
+      }
+  ];
+
+    const links = mockdata.map((item) => <LinksGroup {...item} key={item.label} />);
+   
+    return <div className='admin-panel-container'>
+    <div className='row navbar-row'> 
+        <div className='col-3 sidebar'>
+          <ScrollArea>
+            <div>
+            {links}
+            </div>
+          </ScrollArea>
+        </div>
+        <div className='col-9 content'>
+        </div>
+      </div>
+    </div>
   }
+
+function Calendar(){
+    return (<LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar />
+    </LocalizationProvider>)
+}
