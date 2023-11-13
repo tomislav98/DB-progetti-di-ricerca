16738084356f2b61ab2cbdaf3b7e0d2cbd9dd94b
Cela24k <882778@stud.unive.it>
2023-11-13 19:42:20 +0100

    Document's model: added creation attribute

--- models/project_documents.py
+++ models/project_documents.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from config import db
 
 
@@ -9,10 +10,11 @@ class DocumentProject(db.Model):
     type_document = db.Column(db.String(50), nullable=False)
     version_project_id = db.Column(db.Integer, db.ForeignKey('version_projects.id'))
     pdf_data = db.Column(db.LargeBinary, nullable=False)
-    
+    created=db.Column(db.Date, nullable=False)
+
     @classmethod
-    def create_document(cls, name, type_document, version_project_id, pdf_data):
-        document = cls(name=name, type_document=type_document, version_project_id=version_project_id, pdf_data=pdf_data)
+    def create_document(cls, name, type_document, version_project_id, pdf_data, created=datetime.now().date()):
+        document = cls(name=name, type_document=type_document, version_project_id=version_project_id, pdf_data=pdf_data, created=created)
         db.session.add(document)
         db.session.commit()
     
