c2bbc77fe28cb22adc60894d73b2b40c8bb0bcb2
Cela24k <882778@stud.unive.it>
2023-12-01 21:18:32 +0100

    Admin eval window date range picker added

--- client/react-project/src/Components/MainPage/Admin/CreateWindowStepper.jsx
+++ client/react-project/src/Components/MainPage/Admin/CreateWindowStepper.jsx
@@ -6,26 +6,41 @@ import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { TextField } from '@mui/material';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import dayjs from 'dayjs';
+import { DateField } from '@mui/x-date-pickers';
 
-const steps = ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+const steps = ['Select start date', 'Select end date', 'Create evaluation window'];
 
 function MySteps(props) {
     const [stepperStatus, setStepperStatus] = useState(props.stepperStatus ? props.stepperStatus : { title: '', description: '' })
+    const [startValue, setStartValue] = useState(null);
+    const [endValue, setEndValue] = useState(null);
+
+    const handleEndDateChange = (value)=>{
+        setEndValue(value);
+        props.onFinish();
+    }
 
     switch (props.number) {
         case 1:
             return (
-                <StaticDatePicker/>
+                <div>
+                    <StaticDatePicker defaultValue={dayjs(startValue)} onChange={setStartValue} />
+                </div>
             )
         case 2:
             return (
-                <StaticDatePicker/>
+                <StaticDatePicker defaultValue={dayjs(endValue)} minDate={dayjs(startValue)} onChange={handleEndDateChange} disabled={startValue ? false : true} />
             )
         default:
             return (
-                <div>default step</div>
+                <div>
+                    <h3> Create a new evaluation window </h3>
+                    <DateField defaultValue={dayjs(startValue)} readOnly />
+                    <DateField defaultValue={dayjs(endValue)} readOnly/>
+                </div>
 
             )
     }
@@ -34,10 +49,11 @@ function MySteps(props) {
 export default function CreateWindowStepper() {
     const [activeStep, setActiveStep] = React.useState(0);
     const [skipped, setSkipped] = React.useState(new Set());
-    const [stepperStatus, setStepperStatus] = useState({ title: '', description: '' })
+    const [stepperStatus, setStepperStatus] = useState({ title: '', description: '' }) // forse togliere ?
+    const [canSubmit, setCanSubmit] = useState(false);
 
     const isStepOptional = (step) => {
-        return step === 1;
+        return false; // hard coded a false perche non ci sono steps opzionali
     };
 
     const isStepSkipped = (step) => {
@@ -111,7 +127,7 @@ export default function CreateWindowStepper() {
                 </React.Fragment>
             ) : (
                 <React.Fragment>
-                    <MySteps state={stepperStatus}  number={activeStep + 1} />
+                    <MySteps state={stepperStatus} number={activeStep + 1} onFinish={()=>{setCanSubmit(true)}}/>
                     <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
                     <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
                         <Button
@@ -129,7 +145,7 @@ export default function CreateWindowStepper() {
                             </Button>
                         )}
 
-                        <Button onClick={handleNext}>
+                        <Button onClick={handleNext} disabled={activeStep === steps.length - 1 && !canSubmit}>
                             {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
                         </Button>
                     </Box>
